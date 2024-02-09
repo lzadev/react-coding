d519c689e34ae96fa1b86e1ca37aeb34f02c86d0
Luis Zabala <zabalaluis1234@outlook.com>
2024-02-09 02:16:46 -0400

    finished mouse follower project

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,11 +1,11 @@
-import confetti from 'canvas-confetti';
-import { useState } from 'react';
+import confetti from "canvas-confetti";
+import { useState } from "react";
 
-import { Square } from './components/Square';
-import WinnerModal from './components/WinnerModal';
-import { TURNS } from './constants';
-import { checkEndGame, checkWinner } from './logic/board';
-import { saveGameStorage } from './logic/storage';
+import { Square } from "./components/Square";
+import WinnerModal from "./components/WinnerModal";
+import { TURNS } from "./constants";
+import { checkEndGame, checkWinner } from "./logic/board";
+import { saveGameStorage } from "./logic/storage";
 
 function App() {
   const [board, setBoard] = useState(() => {
@@ -16,8 +16,8 @@ function App() {
   });
 
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem("turn")
-    if (turnFromStorage) return JSON.stringify('turn');
+    const turnFromStorage = window.localStorage.getItem("turn");
+    if (turnFromStorage) return JSON.stringify("turn");
 
     return TURNS.O;
   });
@@ -26,18 +26,17 @@ function App() {
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
-    setTurn(TURNS.O)
+    setTurn(TURNS.O);
     setWinner(null);
     resetGame();
-  }
+  };
 
   const updateBoard = (index) => {
-
     //VALIDAR QUE LA POSICION NO HAYA SIDO TOMADA
     if (board[index] || winner) {
-      alert('THIS POSITION IS TAKEN')
+      alert("THIS POSITION IS TAKEN");
       return;
-    };
+    }
 
     //ACTUALIZAR TABLERO
     const newBoard = [...board];
@@ -56,40 +55,33 @@ function App() {
     if (newWinner) {
       setWinner(newWinner);
       confetti();
-    }
-    else if (checkEndGame(newBoard)) {
+    } else if (checkEndGame(newBoard)) {
       setWinner(false);
     }
-  }
+  };
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1>TIC TAC TOE</h1>
       <button onClick={resetGame}>Resetear la partida</button>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
       </section>
 
-      <section className='turn'>
+      <section className="turn">
         <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
       <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- projects/02-tic-tac-toe/src/components/WinnerModal.jsx
+++ projects/02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { Square } from './Square';
+import React from "react";
+import { Square } from "./Square";
 
 const WinnerModal = ({ winner, resetGame }) => {
-    if (winner === null) return null;
-    const winnerText = winner === false ? 'Empate' : 'Ganó'
-    return (
-        <section className='winner'>
-            <div className='text'>
-                <h2>
-                    {winnerText}
-                </h2>
-                <header className='win'>
-                    {winner && <Square>{winner}</Square>}
-                </header>
-                <footer>
-                    <button onClick={resetGame}>Nueva partida</button>
-                </footer>
-            </div>
-        </section>
-    )
-}
+  if (winner === null) return null;
+  const winnerText = winner === false ? "Empate" : "Ganó";
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
+        <footer>
+          <button onClick={resetGame}>Nueva partida</button>
+        </footer>
+      </div>
+    </section>
+  );
+};
 
-export default WinnerModal
+export default WinnerModal;
